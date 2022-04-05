92ea78f8893a5be7d916efd01285888f869a7d3c
Filippos Makridis <makridistaker@hotmail.gr>
2022-04-05 09:18:28 +0300

    MAINTENANCE: Refactored the code into express app style

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-/*server.js*/
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const app = express();
+const hostname = "127.0.0.1";
 const port = 3000;
 
-function handleRequest (request, response) {
-  response.statusCode = 200;
-  // response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello World\n');
-}
+app.get("/", function handleRequest(request, response) {
+  response.send("Hello World\n");
+});
 
-//This creates the server
-const server = http.createServer(handleRequest);
-
-function listenResponse (){
-  console.log('Server running at http://'+ hostname + ':' + port + '/');
+function listenResponse() {
+  console.log("Server running at http://" + hostname + ":" + port + "/");
 }
 
 //This listens to a specific port and runs a fuction
-server.listen(3000, hostname, listenResponse);
+app.listen(3000, hostname, listenResponse);
