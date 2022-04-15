370bfd7a7ea434f46554d7cf4b4c7f3c2326a501
Filippos Makridis <makridistaker@hotmail.gr>
2022-04-15 22:21:25 +0300

    MAINTENANCE: Refactored the project to use npm packages instead of downloaded ones

---
+++ app.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function (req, res) {
+  const htmlFilePath = path.join(__dirname, "views", "index.html");
+  res.sendFile(htmlFilePath);
+});
+
+function listenResponse() {
+  console.log("Server running at http://" + hostname + ":" + port + "/");
+}
+
+//This listens to a specific port and runs a fuction
+app.listen(3000, hostname, listenResponse);
