0554c71b985d7d41dccca81e04f3885c0bcc9a17
Filippos Makridis <makridistaker@hotmail.gr>
2022-04-20 23:15:47 +0300

    Feature: Added  mysql database integration

---
+++ data/database.js
@@ -0,0 +1,10 @@
+// const mysql = require("mysql2");
+
+// const pool = mysql.createConnection({
+//   host: "localhost",
+//   database: "covid19",
+//   user: "root",
+//   password: "noobaki32"
+// });
+
+// module.exports = pool;
---
+++ routes/covid19.js
@@ -0,0 +1,12 @@
+// const express = require("express");
+
+// const db = require("../data/database");
+
+// const router = express.Router();
+
+// router.get("/", async function (req, res) {
+//   let result = await db.query("SELECT * FROM covid19 WHERE date=" + pickedDate);
+//   console.log(result);
+// });
+
+// module.exports = router;
