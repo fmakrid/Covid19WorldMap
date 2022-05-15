051bb63c3b8d9540f27938d74e00abc77edc1aad
Filippos Makridis <makridistaker@hotmail.gr>
2022-05-15 22:16:47 +0300

    MAINTENANCE: Cleaned up the code and added comments

--- app.js
+++ app.js
@@ -10,14 +10,13 @@ app.use(express.urlencoded({ extended: false }));
 const hostname = "127.0.0.1";
 const port = 3000;
 
+//Database connection
 const connection = mysql.createConnection({
   host: "localhost",
   database: "covid19",
   user: "root",
   password: "noobaki32",
 });
-
-var pickedDate = "2020-12-14";
 
 
 //This runs an sql query, cleans the results and outputs them in the specified file as needed
