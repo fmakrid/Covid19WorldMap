5f0fabe9fc4fe293f7a9ceeb863b91d42e0dd623
Filippos Makridis <makridistaker@hotmail.gr>
2022-06-22 09:40:59 +0300

    MAINTENANCE: Cleaned up the code

--- app.js
+++ app.js
@@ -21,24 +21,22 @@ const connection = mysql.createConnection({
   password: "covid19userpass",
 });
 
-
 //This runs an sql query, cleans the results and outputs them in the specified file as needed
 function sqlQuery(data, callback) {
-  var sql = "SELECT iso2, cases FROM covid19 WHERE dates='" + data + "';"
+  var sql = "SELECT iso2, cases FROM covid19 WHERE dates='" + data + "';";
   connection.query(sql, function (err, results, fields) {
     if (err) {
-        throw err;
-      }
-
-      for (var i of results) {
-        var test = i.iso2;
-        var test2 = i.cases;
-        gdpData[test] = test2;
-      }
-      stuffIWant = gdpData;
-      return callback(gdpData);
+      throw err;
     }
-  );
+
+    for (var i of results) {
+      var test = i.iso2;
+      var test2 = i.cases;
+      gdpData[test] = test2;
+    }
+    stuffIWant = gdpData;
+    return callback(gdpData);
+  });
 }
 
 app.use(express.static("public"));
@@ -49,24 +47,17 @@ app.get("/", function (req, res) {
 });
 
 //This code runs when a POST request is received on /ajaxcall url
-//In this case we receive a post request containing the date picked on calendar, 
+//In this case we receive a post request containing the date picked on calendar,
 //run a query using the date and send the results to the client to color the map accordingly
 app.post("/ajaxcall", function (req, res) {
-  var stuffIWant = {Test:"Not working"}
+  var stuffIWant = { Test: "Not working" };
   var date = req.body.Date;
-  sqlQuery(date, function(result){
+  sqlQuery(date, function (result) {
     stuffIWant = result;
     console.log(stuffIWant);
     res.send(stuffIWant);
   });
 });
-
-// app.use(function (error, req, res, next) {
-  // Default error handling function
-  // Will become active whenever any route / middleware crashes
-//   console.log(error);
-//   res.status(500).render("500");
-// });
 
 function listenResponse() {
   console.log("Server running at " + hostname + ":" + port + "/");
@@ -74,4 +65,3 @@ function listenResponse() {
 //This listens to a specific port if the environment gives one, else it defaults to 3000 and runs a fuction when it is accessed
 // const port = process.env.PORT || 3000;
 app.listen(8080, listenResponse);
-//test2
