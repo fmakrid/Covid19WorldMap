330edd1d106a6c8c7cfe1475dd1bcf4f9046595b
Filippos Makridis <philippos.makridis@three-pi.com>
2023-04-16 19:12:10 +0300

    Refactored index.html to work without server correctly Refactored jQuery to work without server correctly Changed ajax calls to call Rest API point Added code after ajax call to clean results as needed to populate the map
    Removed unused app.js file
    Removed package-lock.json
    Removed package.json
    
    Added global gitignore for node project

--- public/scripts/jQueryDP.js
+++ public/scripts/jQueryDP.js
@@ -1,37 +1,40 @@
 $("document").ready(function ($) {
-  $("#date").datepicker({
-    showOtherMonths: true,
-    selectOtherMonths: true,
-    changeMonth: true,
-    changeYear: true,
-    dateFormat: "yy-mm-dd",
-    //Minimum date based on first Covid-19 case in database, starts from 0, month 1 is February
-    minDate: new Date(2020, 1, 25),
-    //Maximum date based on first Covid-19 case in database, start from 0, month 10 is November
-    maxDate: new Date(2020, 10, 21),
-  });
+	$("#date").datepicker({
+		showOtherMonths: true,
+		selectOtherMonths: true,
+		changeMonth: true,
+		changeYear: true,
+		dateFormat: "yy-mm-dd",
+		//Minimum date based on first Covid-19 case in database, starts from 0, month 1 is February
+		minDate: new Date(2020, 1, 25),
+		//Maximum date based on first Covid-19 case in database, start from 0, month 10 is November
+		maxDate: new Date(2020, 10, 21),
+	});
 });
-
-
 //Prints date when a date is selected or changed
 $(function () {
-  $("#date").on("change", function(){
-    var selectedDate = $(this).val();
-    var pickedDate = selectedDate;
-    // console.log(pickedDate);
-    $.ajax({
-      type: "POST",
-      url: "/ajaxcall",
-      data: {Date: pickedDate},      
-    })
-    .done(function(data){
-      console.log(data);
-      var mapObj = $("#world-map-gdp").vectorMap("get", "mapObject");
-      mapObj.series.regions[0].setValues(data);
-            
-    })
-    .fail( function(xhr, textStatus, errorThrown) {
-      alert(xhr.responseText);
-    }); 
-})
+	$("#date").on("change", function () {
+		var selectedDate = $(this).val();
+		var pickedDate = selectedDate;
+		$.ajax({
+			type: "GET",
+			url: `http://207.154.212.148:8080/api/data/?date="${pickedDate}"`
+		})
+			.done(function (data) {
+				var mapObj = $("#world-map-gdp").vectorMap("get", "mapObject");
+				const gdpDataClean = Object.create(null);
+
+				for (var i of data) {
+					var test = i.iso2;
+					var test2 = i.cases;
+					gdpDataClean[test] = test2;
+				}
+				var stuffIWant = gdpDataClean;
+				console.log(stuffIWant);
+				mapObj.series.regions[0].setValues(stuffIWant);
+			})
+			.fail(function (xhr, textStatus, errorThrown) {
+				alert(xhr.responseText);
+			});
+	});
 });
