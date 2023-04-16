ab654c631da87ab0b0427201d7fee331f1e73e5d
Filippos Makridis <philippos.makridis@three-pi.com>
2023-04-16 19:14:39 +0300

    Minor code formating for visual improvement on readability

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,23 +1,27 @@
 //Creates the map and initializes it
-$(document).ready(function($){
-    $("#world-map-gdp").vectorMap({
-        map: "world_mill_en",
-        zoomButtons : false,
-        zoomOnScroll: false,
-        series: {
-          regions: [
-            {
-              values:  gdpData,
-              scale: ["#FFFFFF","#C23030", "#3D0000","#000000"],
-              normalizeFunction: "polynomial",
-              attribute: 'fill',
-            },
-          ],
-        },
-        backgroundColor: "",
-        onRegionTipShow: function(e, el, code){
-          el.html(el.html()+' Cases '+ $("#world-map-gdp").vectorMap("get", "mapObject").series.regions[0].values[code]);
-        },
-      });
+$(document).ready(function ($) {
+	$("#world-map-gdp").vectorMap({
+		map: "world_mill_en",
+		zoomButtons: false,
+		zoomOnScroll: false,
+		series: {
+			regions: [
+				{
+					values: gdpData,
+					scale: ["#FFFFFF", "#C23030", "#3D0000", "#000000"],
+					normalizeFunction: "polynomial",
+					attribute: "fill",
+				},
+			],
+		},
+		backgroundColor: "",
+		onRegionTipShow: function (e, el, code) {
+			el.html(
+				el.html() +
+					" Cases " +
+					$("#world-map-gdp").vectorMap("get", "mapObject").series
+						.regions[0].values[code]
+			);
+		},
+	});
 });
-
